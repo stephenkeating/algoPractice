fa2b0ffa9a74888af00e86ebd9b2b3d02e707d63
Stephen Keating <stephen.keating@gmail.com>
2020-01-10 15:13:11 -0500

    did pyramid problem

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,51 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// our solution to steps:
+// function steps(n) {
+//   let pounds = ''
+//   let spaces = ' '
+  
+//   for (i = 1; i <= n; i++) {
+//     pounds = pounds + '#'
+//     // for (k = n-i; k > 0; k--) {
+//       // (spaces = spaces + " ") * n-i
+//     // }
+//     console.log(pounds + spaces.repeat(n-i))
+//   }
+// }
+
+// will need to use 2x-1 to solve levels
+
+// row 1 will always be 1 # with (2n-2)/2 spaces *on each side* of the #
+// if row 2 exists, it is 2n-1 #s with 
+// level n will always be 2n-1 #s
+
+// let pounds = ''
+// let space = ' '
+// loop from 0 to n. 
+// for (i = 1; i <= n.length; i++) {
+  // pounds = pounds + '#'
+  // console.log(space.repeat(some logic) + pounds + space.repeat(some logic))
+// }
+
+function pyramid(n) {
+
+  let pounds = '#'
+  let space = ' '
+  let maxLength = (2 * n - 1)
+  
+  for (let i = 1; i <= n; i++) {
+    let poundsStr = pounds.repeat(2 * i - 1)
+    console.log(space.repeat((maxLength - poundsStr.length)/2) + poundsStr + space.repeat((maxLength - poundsStr.length)/2))
+  }
+
+}
 
 module.exports = pyramid;
+
+//       ' # '
+//       '###'
+//       '  #  '
+//       ' ### '
+//       '#####'   // n = 3; poundStr.length = 5
