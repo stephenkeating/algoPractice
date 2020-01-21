b517e5946b04cc2df27efeaf4fb95b6c8d1613e7
Stephen Keating <stephen.keating@gmail.com>
2020-01-21 17:16:15 -0500

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,32 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // Regex: g makes sure we dont stop at first match. i makes it case insensitive.
+  // matches returns an array if it finds any. returns null if not. 
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+//   let total = 0
+//   for (let char of str.toLowerCase()) {
+//     if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//       total++
+//     }
+//   }
+//   return total
+// }
+
+// function vowels(str) {
+//   let total = 0
+//   let checker = 'aeiou'
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       total++
+//     }
+//   }
+//   return total
+// }
