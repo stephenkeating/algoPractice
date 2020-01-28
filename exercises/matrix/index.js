9f43a9a5ec217e685383e88295652c4ec798417f
Stephen Keating <stephen.keating@gmail.com>
2020-01-28 09:28:27 -0500

    working

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,34 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// need start/end column vars, and start/end row vars. and counter var. 
+
+// create mainArray
+// create counter = 1
+// as long as start column <= end column && start row <= end row
+  // loop from start column to end column
+    // at mainArray[start column][i] assign counter
+    // counter++
+  // increment start row
+  // loop from start row to end row
+    // at mainArray[i][end column] assign counter variable
+    // counter++
+  // decrement end row
+  // ... repeat for other two sides
+
+function matrix(n) {
+
+  let counter = 1
+  let startRow = 1
+  let startColumn = 1
+  let endRow = n
+  let endColumn = n
+
+  while (startColumn <= endColumn && startRow <= endRow)
+    for (let i = 0; i < endRow; ) {
+
+    }
+
+}
 
 module.exports = matrix;
