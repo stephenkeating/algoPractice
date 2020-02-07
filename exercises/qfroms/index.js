de8ad1dfae3e6af0bf9750c0848920405d388f32
Stephen Keating <stephen.keating@gmail.com>
2020-02-07 15:07:40 -0500

    qfroms completed

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,51 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+
+  constructor() {
+    this.first = new Stack ();
+    this.second = new Stack ();
+  }
+
+  add(record) {
+    // some logic that mimics a normal queue add
+    this.first.push(record)
+  }
+
+  remove() {
+    // some logic that mimics a normal queue remove
+    while (this.first.peek()) {
+      this.second.push(this.first.pop())
+    }
+    const recordToRemove = this.second.pop()
+    while (this.second.peek()) {
+      this.first.push(this.second.pop())
+    }
+    return recordToRemove;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop())
+    }
+    const recordToPeek = this.second.peek()
+    while (this.second.peek()) {
+      this.first.push(this.second.pop())
+    }
+    return recordToPeek;
+  }
+
+  // normal queue add
+//   add(record) {
+//     this.data.unshift(record);
+//   }
+
+// normal queue remove
+//   remove() {
+//     return  this.data.pop();
+//   }
+
+}
 
 module.exports = Queue;
