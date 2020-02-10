017a5ec13a1e2d6dec067cfc8a610e6759e84bde
Stephen Keating <stephen.keating@gmail.com>
2020-02-09 23:57:27 -0500

    linked list work

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,12 +3,30 @@
 // See 'directions' document
 
 class Node {
-  constructor (data, next = null) {
-    this.data = data
-    this.next = next
+  constructor (data, next = null ) {
+    this.data = data;
+    this.next = next;
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+
+  insertFirst (data) {
+    this.head = new Node (data, this.head);
+  }
+
+  size () {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter
+  }
+}
 
 module.exports = { Node, LinkedList };
