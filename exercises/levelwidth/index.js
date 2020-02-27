ea12575f04768d1ea63041b90172ac311f567f4c
Stephen Keating <stephen.keating@gmail.com>
2020-02-27 11:43:58 -0500

    tree width completed

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let arr = [root, 's'];
+  let result = [0];
+
+  while (arr.length > 1) {
+    let node = arr.shift();
+    if (node === 's') {
+      arr.push(node);
+      result.push(0);
+    } else {
+      arr.push(...node.children);
+      result[result.length - 1]++;
+    }
+  }
+
+  return result;
+}
 
 module.exports = levelWidth;
