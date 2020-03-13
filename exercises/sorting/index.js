7f67a9be2c6877b767d337b2ba3c7739a8d3df3f
Stephen Keating <stephen.keating@gmail.com>
2020-03-12 20:55:55 -0400

    selection sort finished

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -17,8 +17,35 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {
+// selectionSort psuedocode
+// initialize i = 0, j = 0, indexOfMin = 0
+// from i = 0 < array.length
+  // assume arr[i] is the least in the array, assign i to to indexOfMin
+  // for j from i + 1 to end of array
+    // see if there is an element with a lower value
+      // if there is, record its index
+  // if the index of the current element and the lowest element are not the same, swap them
 
+function selectionSort(arr) {
+  let i = 0;
+  let indexOfMin = 0;
+  
+  while (i < arr.length) {
+    let j = i + 1;
+    while (j < arr.length) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+      j++;
+    }
+    if (arr[i] > arr[indexOfMin]) {
+      let min = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = min;
+    }
+    i++;
+  }
+  return arr
 }
 
 function mergeSort(arr) {
